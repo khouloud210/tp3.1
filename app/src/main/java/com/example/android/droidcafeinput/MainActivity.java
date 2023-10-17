b447b89f0ce2c2921a0fee438e7dd03a04a511be
POST4 <post4.prog2@enetcom.usf.tn>
2023-10-17 16:53:48 +0100

    tp3.1

--- app/src/main/java/com/example/android/droidcafeinput/MainActivity.java
+++ app/src/main/java/com/example/android/droidcafeinput/MainActivity.java
@@ -88,18 +88,10 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        // This comment suppresses the Android Studio warning about simplifying
-        // the return statements.
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_order) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
